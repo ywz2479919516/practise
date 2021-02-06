5f19b17a69b711d0679c708f85b9608d2b174f42
yuanweizheng <2>
2021-02-06 10:18:22 +0800

    练习二第1关

--- practise/src/router/index.js
+++ practise/src/router/index.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import App1 from '../App1'
+import App2 from '../App2'
+import App3 from '../App3'
+import select from '../select'
 import practise1 from '../components/practise1'
 import practise2 from '../components/practise2'
 
@@ -13,12 +17,33 @@ export default new Router({
   mode: 'history',
   routes: [
     {
+      path: '/',
+      name: 'select',
+      component: select
+    },
+    {
+      path: '/app1',
+      name: 'App1',
+      component: App1
+    },
+    {
+      path: '/app2',
+      name: 'App2',
+      component: App2
+    },
+    {
+      path: '/app3',
+      name: 'App3',
+      component: App3
+    },
+    {
       path: '/p1',
       name: 'p1',
       component: practise1
     },
     {
       path: '/p2',
+      name: 'p2',
       component: practise2
     }
   ]
